813e7f2779a90848bbdf574802332c1a5d1904c5
Buiscki1 <122836250+Buiscki1@users.noreply.github.com>
2023-04-20 00:43:50 +0200

    Iteration 3

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -2,13 +2,25 @@ const Celebrity = require("../models/Celebrity.model");
 
 const router = require("express").Router();
 
-router.get("/create", (req, res, next) => {
-    res.render("celebrities/new-celebrity")
-})
+router.get("/", (req, res, next) => {
+  res.render("celebrities/celebrities");
+  console.log("test");
+});
 
-router.get("/celebrities", (req, res, next) => {
-    res.render("celebrities/celebrities")
-    console.log("okay")
+router.get("/create", (req, res, next) => {
+  res.render("celebrities/new-celebrity");
+  console.log("test");
+});
+
+router.post("/create", async (req, res, next) => {
+  const { name, occupation, catchPhrase } = req.body;
+  try {
+    await Celebrity.create({ name, occupation, catchPhrase });
+    res.redirect("/celebrities");
+  } catch (err) {
+    console.log(err);
+    res.redirect("/celebrities/new-celebrity");
+  }
 });
 
 module.exports = router;
