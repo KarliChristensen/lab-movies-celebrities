d49b03d6f78abb1fac737bea078b109e293fa61e
Buiscki1 <122836250+Buiscki1@users.noreply.github.com>
2023-04-20 16:03:13 +0200

    Iteration 4

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -2,14 +2,20 @@ const Celebrity = require("../models/Celebrity.model");
 
 const router = require("express").Router();
 
-router.get("/", (req, res, next) => {
-  res.render("celebrities/celebrities");
-  console.log("test");
+router.get("/", async (req, res, next) => {
+  Celebrity.find()
+    .then((CelebritiesFromDatabase) => {
+      res.render("celebrities/celebrities", {
+        allcelebrities: CelebritiesFromDatabase,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
 
 router.get("/create", (req, res, next) => {
   res.render("celebrities/new-celebrity");
-  console.log("test");
 });
 
 router.post("/create", async (req, res, next) => {
